47b36b192e71c8e7025c4be108feb6f3082d08c0
Rami Mohamed <rami.workstation@gmail.com>
2026-08-31 00:07:22 +0400

    test: isolate transfer process manager integration db

--- src/test/java/com/digitalbank/transactionservice/application/service/TransferProcessManagerSpringIT.java
+++ src/test/java/com/digitalbank/transactionservice/application/service/TransferProcessManagerSpringIT.java
@@ -10,15 +10,25 @@ import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 class TransferProcessManagerSpringIT {
+
+    private static final String TEST_DATABASE =
+            "jdbc:h2:mem:transfer-process-manager-spring-it-" + UUID.randomUUID() + ";MODE=PostgreSQL;DB_CLOSE_DELAY=-1";
 
     @Autowired
     private TransferProcessManager processManager;
 
     @Autowired
     private TransferWorkflowRepository workflowRepository;
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> TEST_DATABASE);
+    }
 
     @Test
     void springWiringPersistsInitialTransferAndAction() {
